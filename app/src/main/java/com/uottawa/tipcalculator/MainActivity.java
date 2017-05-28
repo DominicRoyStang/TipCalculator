322eb9d1e94b23f8ee8b635f2d8e4bf0ea5109ac
Dominic Roy-Stang <dominicroystang@gmail.com>
2017-05-28 03:07:47 -0400

    Updated the actionbar so it uses triple dots. Displays Settings and About screen. New settings activity (java) created, not implemented yet.

--- app/src/main/java/com/uottawa/tipcalculator/MainActivity.java
+++ app/src/main/java/com/uottawa/tipcalculator/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,13 +21,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // set custom action bar
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.mainmenu_layout);
 
+
         // get setting button and assign its onClick()
-        ImageButton settingsButton = (ImageButton) findViewById(R.id.SettingsImageButton);
-        settingsButton.setOnClickListener(this); // calling onClick() method
+        //ImageButton settingsButton = (ImageButton) findViewById(R.id.SettingsImageButton);
+        //settingsButton.setOnClickListener(this); // calling onClick() method
 
         EditText billEditText = (EditText) findViewById(R.id.BillEditText);
         billEditText.addTextChangedListener(new TextWatcher() {
@@ -55,23 +59,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch(v.getId()){
+            /*
             case R.id.SettingsImageButton:
                 Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
                 startActivity(myIntent);
                 break;
+            */
             case R.id.CalculateButton:
                 Toast.makeText(getApplicationContext(), "You pressed the calculate button", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
 
-    /*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_name, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }*/
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_settings:
+                Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(myIntent);
+                break;
+            case R.id.menu_about:
+                Toast.makeText(getApplicationContext(), "You pressed the About button", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
 
 
 }
